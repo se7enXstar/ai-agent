23da0d230e6ddd21a6eeb247089b4c2aa6aa182b
hugehugo <huanghuoyu@gmail.com>
2024-06-12 21:54:52 -0400

    modify

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,25 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/websev/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            ? "http://127.0.0.1:1234/websev/:path*"
+            : "/websev/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            ? "http://127.0.0.1:1234/docs"
+            : "/websev/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            ? "http://127.0.0.1:1234/openapi.json"
+            : "/websev/openapi.json",
       },
     ];
   },
